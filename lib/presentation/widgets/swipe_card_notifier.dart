f46b65639f14ebc1a5576ebd258490d7214f224c
Duy Huynh <htduykhtn@gmail.com>
2024-09-13 14:00:07 +0700

    Remove unused file

--- lib/presentation/widgets/swipe_card_notifier.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:tinder_discover/presentation/widgets/card_action.dart';
-
-class SwipeCardNotifier extends ChangeNotifier {
-  CardAction? hoveredAction;
-
-  
-}
